1b52ca3f25d32a23281277ffadc30193eda55c43
Diego <diegoteixeirapereira@hotmail.com>
2021-03-12 09:26:34 -0300

    criação do arquivo classe que verifica o tamanho atual da lista.

---
+++ javaExemplosBasicos/src/br/com/java/testes/TesteTamanhoLista.java
@@ -0,0 +1,29 @@
+package br.com.java.testes;
+
+import br.com.java.modelo.Aluno;
+import br.com.java.vetor.Vetor;
+
+public class TesteTamanhoLista {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Aluno a1 = new Aluno();
+		Aluno a2 = new Aluno();
+		Aluno a3 = new Aluno();
+		
+		a1.setNome("Rafael");
+		a2.setNome("Paulo");
+		
+		Vetor lista = new Vetor();
+		
+		lista.adiciona(a1);
+		lista.adiciona(a2);
+		
+		System.out.println(lista.tamanho());
+		
+		lista.adiciona(a3);
+		
+		System.out.println(lista.tamanho());
+	}
+
+}
