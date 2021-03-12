4367e4474ec18cd4d491a7514ed6430b7e036750
Diego <diegoteixeirapereira@hotmail.com>
2021-03-12 11:47:50 -0300

    alteração do arquivo para teste

--- javaExemplosBasicos/src/br/com/java/testes/Teste.java
+++ javaExemplosBasicos/src/br/com/java/testes/Teste.java
@@ -3,22 +3,33 @@ package br.com.java.testes;
 import java.util.ArrayList;
 
 import br.com.java.modelo.Aluno;
+import br.com.java.vetor.Vetor;
 
 public class Teste {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		ArrayList vetorSemGenerics = new ArrayList();
-		ArrayList<Aluno> vetorComGenerics = new ArrayList<Aluno>();
+		Aluno a1 = new Aluno();
+		a1.setNome("Rafael");
 		
-		Aluno aluno = new Aluno();
+		Vetor lista = new Vetor();
 		
-		vetorSemGenerics.add(aluno);
-		vetorComGenerics.add(aluno);
+		lista.adiciona(a1);
 		
-		Aluno a1 = (Aluno) vetorSemGenerics.get(0);
-		Aluno a2 = vetorComGenerics.get(0);
+		lista.pega(0);
+		lista.pega(1);
+		lista.pega(100000000);
+//		ArrayList vetorSemGenerics = new ArrayList();
+//		ArrayList<Aluno> vetorComGenerics = new ArrayList<Aluno>();
+//		
+//		Aluno aluno = new Aluno();
+//		
+//		vetorSemGenerics.add(aluno);
+//		vetorComGenerics.add(aluno);
+//		
+//		Aluno a1 = (Aluno) vetorSemGenerics.get(0);
+//		Aluno a2 = vetorComGenerics.get(0);
 //		ArrayList vetor = new ArrayList();
 //		
 //		Aluno aluno1 = new Aluno();
