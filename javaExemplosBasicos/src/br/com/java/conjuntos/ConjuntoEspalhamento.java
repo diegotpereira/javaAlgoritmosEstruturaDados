c99f14275f885916c0838519e0b0eac11659efd4
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-03-16 16:56:03 -0300

    alteração no arquivo classe de operações de conjunto de espalhamento.

--- javaExemplosBasicos/src/br/com/java/conjuntos/ConjuntoEspalhamento.java
+++ javaExemplosBasicos/src/br/com/java/conjuntos/ConjuntoEspalhamento.java
@@ -1,6 +1,7 @@
 package br.com.java.conjuntos;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -8,6 +9,7 @@ public class ConjuntoEspalhamento {
 	
 	private ArrayList<LinkedList<String>> tabela = new ArrayList<LinkedList<String>>();
 //	private List<List<String>>tabela = new ArrayList<<List<String>>();
+	private int tamanho = 0;
 	
 	public ConjuntoEspalhamento() {
 		for (int i = 0; i < 26; i++) {
@@ -16,12 +18,16 @@ public class ConjuntoEspalhamento {
 		}
 	}
 	private int calculaIndiceDaTabela(String palavra) {
-		return palavra.toLowerCase().charAt(0) % 26;
+//		return palavra.toLowerCase().charAt(0) % 26;
+		int codigoDeEspelhamento = this.calculaCodigoDeEspalhamento(palavra);
+		codigoDeEspelhamento = Math.abs(codigoDeEspelhamento);
+		return codigoDeEspelhamento % this.tabela.size();
 	}
 	
 	public void adiciona(String palavra) {
 		
 		if (!this.contem(palavra)) {
+			this.verificaCarga();
 			int indice = this.calculaIndiceDaTabela(palavra);
 			List<String>lista = this.tabela.get(indice);
 			lista.add(palavra);
@@ -31,9 +37,11 @@ public class ConjuntoEspalhamento {
 	
 	public void remove(String palavra) {
 		if (this.contem(palavra)) {
+			this.verificaCarga();
 			int indice = this.calculaIndiceDaTabela(palavra);
 			List<String> lista = this.tabela.get(indice);
 			lista.remove(palavra);
+			this.tamanho--;
 		}
 	}
 	
@@ -42,8 +50,54 @@ public class ConjuntoEspalhamento {
 		List<String> lista = this.tabela.get(indice);
 		return lista.contains(palavra);
 	}
+	public List<String>pegaTodas(){
+		List<String>palavras = new ArrayList<String>();
+		
+		for (int i = 0; i < this.tabela.size(); i++) {
+			palavras.addAll(this.tabela.get(i));
+		}
+		return palavras;
+	}
 	
 	public int tamanho() {
-		return 0;
+		return this.tamanho;
+	}
+	private int calculaCodigoDeEspalhamento(String palavra) {
+		int codigo = 1;
+		for(int i =0; i < palavra.length(); i++) {
+			codigo = 31 * codigo + palavra.charAt(i);
+		}
+		return codigo;
+	}
+	public void imprimiTabela() {
+		for(List<String> lista : this.tabela) {
+			System.out.println("[");
+			for(int i = 0; i < lista.size(); i++) {
+				System.out.print("*");
+			}
+			System.out.println("]");
+		} 
+	}
+	private void redimensionaTabela(int novaCapacidade){
+		List<String>palavras = this.pegaTodas();
+		this.tabela.clear();
+		
+		for(int i = 0; i < novaCapacidade; i++) {
+			this.tabela.add(new LinkedList<String>());
+		}
+		for(String palavra : palavras) {
+			this.adiciona(palavra);
+		}
+	}
+	private void verificaCarga() {
+//		int capacidade = this.tabela.size();
+		
+		double carga = (double)this.tamanho / this.tabela.size();
+		
+		if (carga > 0.75) {
+			this.redimensionaTabela(this.tamanho * 2);
+		}else if (carga < 0.25) {
+			this.redimensionaTabela(Math.max(this.tamanho / 2, 10));
+		}
 	}
 }
