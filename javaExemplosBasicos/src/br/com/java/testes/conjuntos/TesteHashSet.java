46a52eee27ba4a52d9a936f1369a7013ebf5d179
Diego <diegoteixeirapereira@hotmail.com>
2021-03-18 10:51:53 -0300

    criação do arquivo classe de teste HasSet.

---
+++ javaExemplosBasicos/src/br/com/java/testes/conjuntos/TesteHashSet.java
@@ -0,0 +1,18 @@
+package br.com.java.testes.conjuntos;
+
+import java.util.HashSet;
+
+public class TesteHashSet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashSet conjunto = new HashSet();
+		conjunto.add("Rafael");
+		conjunto.add("Rafael");
+		conjunto.add("Ana");
+		conjunto.add("Paulo");
+		
+		System.out.println(conjunto);
+	}
+
+}
