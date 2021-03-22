296c1fc52319f10714ec03bcc7b464e6aa658565
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-03-22 09:44:30 -0300

    alteração no arquivo classe associacao criado metodo construtor.

--- javaExemplosBasicos/src/br/com/java/mapas/Associacao.java
+++ javaExemplosBasicos/src/br/com/java/mapas/Associacao.java
@@ -6,6 +6,11 @@ public class Associacao {
 	
 	private String placa;
 	private Carro carro;
+	
+	public Associacao(String placa, Carro carro) {
+		this.placa = placa;
+		this.carro = carro;
+	}
 	public String getPlaca() {
 		return placa;
 	}
