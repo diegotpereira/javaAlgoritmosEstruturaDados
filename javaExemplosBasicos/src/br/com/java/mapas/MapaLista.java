93371cc5e3ec539753b0fa7b32609150662fca1d
Diego <diegoteixeirapereira@hotmail.com>
2021-03-22 09:21:49 -0300

    criação do arquivo classe java de operação.

---
+++ javaExemplosBasicos/src/br/com/java/mapas/MapaLista.java
@@ -0,0 +1,17 @@
+package br.com.java.mapas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.java.modelo.Carro;
+
+public class MapaLista {
+	
+	private List<Associacao>associacoes = new ArrayList<Associacao>();
+	
+	public void adiciona(String placa, Carro carro) {
+		if (!this.contemChave(placa)) {
+			Associacao associacao = new Associacao?()
+		}
+	} 
+}
