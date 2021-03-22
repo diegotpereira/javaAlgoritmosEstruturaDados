6bab7ff30ceaea96efdf725f838bd80554302485
Diego <diegoteixeirapereira@hotmail.com>
2021-03-22 09:21:05 -0300

    criação do arquivo classe java de operação Associacao.

---
+++ javaExemplosBasicos/src/br/com/java/mapas/Associacao.java
@@ -0,0 +1,21 @@
+package br.com.java.mapas;
+
+import br.com.java.modelo.Carro;
+
+public class Associacao {
+	
+	private String placa;
+	private Carro carro;
+	public String getPlaca() {
+		return placa;
+	}
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+	public Carro getCarro() {
+		return carro;
+	}
+	public void setCarro(Carro carro) {
+		this.carro = carro;
+	}
+}
