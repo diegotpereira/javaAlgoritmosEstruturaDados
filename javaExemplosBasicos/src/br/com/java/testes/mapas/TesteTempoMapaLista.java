466437a2c4fa05dbf52c5fde8f60e08c748b9621
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-03-22 09:43:56 -0300

    criação do arquivo classe teste tempo mapa lista

---
+++ javaExemplosBasicos/src/br/com/java/testes/mapas/TesteTempoMapaLista.java
@@ -0,0 +1,31 @@
+package br.com.java.testes.mapas;
+
+import br.com.java.mapas.MapaLista;
+import br.com.java.modelo.Carro;
+
+public class TesteTempoMapaLista {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MapaLista mapaLista = new MapaLista();
+		int numeroDeElementos = 15000;
+		long inicio = System.currentTimeMillis();
+		
+		for(int i = 0; i < numeroDeElementos; i++) {
+			mapaLista.adiciona("" + i, new Carro("c" + i));
+		}
+		for(int i = 0; i < numeroDeElementos; i ++) {
+			mapaLista.pega("" + i);
+		}
+		for(int i = 0; i < numeroDeElementos; i ++) {
+			mapaLista.contemChave("" + i);
+		}
+		for(int i = 0; i < numeroDeElementos; i++) {
+			mapaLista.remove("" + i);
+		}
+		long fim = System.currentTimeMillis();
+		
+		System.out.println("Tempo: " + (fim - inicio) / 1000.0);
+	}
+
+}
